b00d0553e26f00d889901ea188e930f0df858e8b
mahdiRezai <Engineer.m.rezai@gmail.com>
2024-06-04 14:09:19 +0330

    create StudentPortalDb.db add studenttable

---
+++ Data/AplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentPortal.Models.Entieis;
+
+namespace StudentPortal.Data;
+
+public class AplicationDbContext: DbContext
+{
+    public AplicationDbContext(DbContextOptions<AplicationDbContext> options) :base(options)
+    {
+        
+    }
+
+    public DbSet<students> Students { get; set; }
+}
